5a9749c9b56b16007795177bc40ac1f7bb58f836
Alexey Yurchenko <alexeyyurchenko.java@gmail.com>
2024-03-26 10:32:32 +0700

    next commit

--- integration-app/src/main/java/com/example/service/integrationapp/clients/OkHttpClientSender.java
+++ integration-app/src/main/java/com/example/service/integrationapp/clients/OkHttpClientSender.java
@@ -7,8 +7,12 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -43,7 +47,23 @@ public class OkHttpClientSender {
             return new String(response.body().bytes());
 
         }
-
+    }
+    public Resource downLoadFile(String filename) {
+        Request request = new Request.Builder()
+                .url(baseUrl + "/api/v1/file/download/" + filename)
+                .header("Accept","application/octet-stream")
+                .get()
+                .build();
+        try(Response response = httpClient.newCall(request).execute()){
+            if (!response.isSuccessful()) {
+                log.error("Error trying to download file");
+                return null;
+            }
+            return new ByteArrayResource(response.body().bytes());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 
 }
